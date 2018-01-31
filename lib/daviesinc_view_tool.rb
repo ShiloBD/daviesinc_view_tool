6b62b64bcac8157f44c0437b5e22db9a961cc8b4
ShiloBD <shilo_davies@hotmail.com>
2018-01-31 12:29:36 -0700

    Creating a ruby gem

---
+++ lib/daviesinc_view_tool.rb
@@ -0,0 +1,6 @@
+require "daviesinc_view_tool/version"
+require "daviesinc_view_tool/renderer"
+
+module DaviesincViewTool
+  # Your code goes here...
+end
---
+++ lib/daviesinc_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DaviesincViewTool
+  VERSION = "0.1.0"
+end
